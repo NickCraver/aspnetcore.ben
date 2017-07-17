9ab99c3d3652c4b3d4a481d272e3178debe3a16d
Ben Adams <thundercat@illyriad.co.uk>
2017-07-17 20:01:02 +0100

    Add HttpListener

---
+++ src/AspNetCore.Ben.HttpListener/samples/HttpListenerApplication/HttpContextExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace HttpListenerApplication
+{
+    public static class HttpContextExtensions
+    {
+        public static async Task<string> ReadInputStringAsync(this HttpRequest request)
+        {
+            string result;
+            using (var reader = new StreamReader(request.Body))
+            {
+                result = await reader.ReadToEndAsync();
+            }
+            return result;
+        }
+
+        public static async Task WriteOutputStringAsync(this HttpResponse response, string text)
+        {
+            using (var writer = new StreamWriter(response.Body))
+            {
+                await writer.WriteAsync(text);
+            }
+        }
+    }
+}
